6e00cfece6517c3b8d4fed0b27b082664637458e
Rex Kirshner <github@rexkirshner.com>
2011-09-14 13:41:10 -0700

    list works

--- WaterAppServer/waterapp/models.py
+++ WaterAppServer/waterapp/models.py
@@ -5,20 +5,22 @@ from django.db import models
 class LeakType(models.Model):
 	description = models.CharField(max_length=200)
 	critical_severity = models.IntegerField()
-	severity_list = models.ForeignKey('SeverityList')
-		
+	#severity_list = models.ForeignKey('SeverityList')
+	low = models.ForeignKey('Severity')
+	mid = models.ForeignKey('Severity')
+	high = models.ForeignKey('Severity')
+	
 	
 class EmergencyContact(models.Model):
 	phone_number = models.CharField(max_length=10)
 	email = models.CharField(max_length=30)
 	
-class SeverityList(models.Model):
-	low = models.ForeignKey('Severity')
-	mid = models.ForeignKey('Severity')
-	high = models.ForeignKey('Severity')
 	
 class Severity(models.Model):
 	description = models.CharField(max_length=50) 
+	
+	def natural_key(self):
+		return (self.description)
 	
 class LeakReport(models.Model):
 	leak_type = models.IntegerField()
